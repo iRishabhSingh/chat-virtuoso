817fa89719dc1c11da22939f53b664d0436286d4
iRishabhSingh <rishab18011@gmail.com>
2024-05-07 12:20:04 +0530

    Logout Button updated

--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 import {
   AlertDialog,
   AlertDialogAction,
@@ -16,10 +17,12 @@ import { signOut, useSession } from "next-auth/react";
 import { LoaderCircle, LogOut } from "lucide-react";
 
 const Logout = ({ className }: { className?: string }) => {
+  const [isLoading, setIsLoading] = useState(false);
   const { toast } = useToast();
   const { data: session, status } = useSession();
 
   const handleLogout = async () => {
+    setIsLoading(true);
     try {
       await signOut({
         redirect: false,
@@ -37,47 +40,41 @@ const Logout = ({ className }: { className?: string }) => {
           "We encountered an error while signing out. Please try signing out again.",
       });
     }
+    setIsLoading(false);
   };
 
-  if (status === "loading") {
-    return (
-      <Button disabled className={`w-full max-w-[236px] h-12 ${className}`}>
-        <LoaderCircle className="mr-2 h-4 w-4 animate-spin" />
-        Log out
-      </Button>
-    );
-  } else {
-    return session ? (
-      <AlertDialog>
-        <AlertDialogTrigger
-          asChild
-          className={`w-full max-w-[236px] h-12 ${className}`}
-        >
-          <Button className={`w-full max-w-[236px] h-12 ${className}`}>
+  return session ? (
+    <AlertDialog>
+      <AlertDialogTrigger
+        asChild
+        className={`w-full max-w-[236px] h-12 ${className}`}
+      >
+        <Button className={`w-full max-w-[236px] h-12 ${className}`}>
+          {isLoading ? (
+            <LoaderCircle className="mr-2 h-4 w-4 animate-spin" />
+          ) : (
             <LogOut className="mr-2 h-4 w-4" />
-            Log out
-          </Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
+          )}
+          Log out
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
           <AlertDialogHeader>
-            <AlertDialogHeader>
-              <AlertDialogTitle>Confirm Logout</AlertDialogTitle>
-              <AlertDialogDescription>
-                Are you sure you want to log out? You will need to log in again
-                to continue using our services.
-              </AlertDialogDescription>
-            </AlertDialogHeader>
+            <AlertDialogTitle>Confirm Logout</AlertDialogTitle>
+            <AlertDialogDescription>
+              Are you sure you want to log out? You will need to log in again to
+              continue using our services.
+            </AlertDialogDescription>
           </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction onClick={handleLogout}>
-              Continue
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    ) : null;
-  }
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={handleLogout}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  ) : null;
 };
 
 export default Logout;
